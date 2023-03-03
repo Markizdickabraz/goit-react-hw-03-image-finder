9a43448e939a5696dbd907fbba71da6d179dd67c
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-03-03 08:08:05 +0200

    fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,52 +5,80 @@ import Searchbar from './Searchbar/Searchbar'
 import ImageGallary from './ImageGallery/ImageGallery'
 import Modal from './Modal/Modal'
 import LoadMore from './Button/Button'
+import axios from 'axios'
+import Loader from 'components/Loader/Loader'
 
 
-const askServer = new ImageGallary();
+
 
 class App extends Component {
   state = {
     name: '',
-    data: null,
+    data: [],
     showModal: false,
     largeUrl: null,
     alt: null,
     loading: false,
-    perPage: 12
+    perPage: 12,
+    page: 1
   }
   
+  async fetch() {
+    this.setState({
+      loading: true
+   })
+      const BASEURL = 'https://pixabay.com/api/';
+      try {
+        const response = await axios.get(`${BASEURL}?key=32463298-aa2adc14f1416dd47ab6801d7&q=${this.state.name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.state.perPage}&page=${this.state.page}`);
+        const responseData = await response.data.hits;
+        console.log(responseData)
+        this.setState({
+          data: responseData
+        })
+    
+      } catch (error) {
+        console.log(error);
+      }
+        this.setState({loading:false})
+  }
+
     toggleModal = () => this.setState(({showModal}) => ({
     showModal: !showModal
   }))
   
 
   modalItems = (dataFind) => {
-    console.log(dataFind)
     this.setState({
       largeUrl: dataFind.largeImageURL,
-      alt: dataFind.tags
+      alt: dataFind.tags,
   })
   }
 
   onSubmit = (data) => {
     this.setState({
       name: data.name})
-    
   }
 
   loadMoreClick = (data) => {
-      console.log(data)
     this.setState(prevState => ({
       perPage: prevState.perPage + Number(data.perPage)
     }))
   }
   
   componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.perPage);
+    console.log(prevState.perPage)
         if (this.state.perPage !== prevState.perPage) {
-          askServer.fetch();
-     
+          this.fetch();
+        }
+    
+        if (this.props.name !== prevProps.name) {
+           this.setState({
+            perPage: 12,
+      })
+          this.fetch();
     }
+
   }
 
   render() {
@@ -58,10 +86,10 @@ class App extends Component {
       <AppStyled>
         <GlobalStyle />
         <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallary name={this.state.name} toggleModal={this.toggleModal } modalItems= {this.modalItems} /> 
+        <ImageGallary data={this.data} toggleModal={this.toggleModal}/> 
         {this.state.showModal && <Modal onClick={() => { this.toggleModal()}} src ={this.state.largeUrl} alt={this.state.alt}></Modal>}
         {this.state.data !== null && <LoadMore click={this.loadMoreClick} />}
-
+        {this.state.loading && <Loader /> }
       </AppStyled>
     );
   }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,53 +1,19 @@
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem'
 import React, { Component } from 'react'
 import { GalleryStyled } from './ImageGalleryStyled'
-import axios from 'axios'
-import Loader from 'components/Loader/Loader'
 
-
-
-class ImageGallary extends Component {
-    state = {
-        page: 1,
-        perPage: 12,
+export default class ImageGallary extends Component {
+  state = {
         data: null,
         loading: false,
+        name: ''
     }
     
-  async fetch() {
-    this.setState({
-      loading: true
-   })
-      const BASEURL = 'https://pixabay.com/api/';
-      try {
-        const response = await axios.get(`${BASEURL}?key=32463298-aa2adc14f1416dd47ab6801d7&q=${this.props.name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.state.perPage}&page=${this.state.page}`);
-        const responseData = await response.data.hits;
-        this.setState({
-          data: responseData
-        })
-    
-      } catch (error) {
-        console.log(error);
-      }
-        this.setState({loading:false})
-  }
+  // componentDidMount() {
+  //   this.setState({data: this.props.data})
+  // }
   
-  async componentDidUpdate(prevProps, prevState) {
-
-    if (this.props.name !== prevProps.name) {
-      this.setState({
-        perPage: 12
-      })
-        this.fetch()
-    }
-
-  }
-
-  
-
-
   galleryItemClick =(e) => {
-    console.dir(e.target)
     if (e.target.nodeName !== 'IMG') {
       return;
     }
@@ -60,13 +26,11 @@ class ImageGallary extends Component {
     render() {
         return (
             <div>
-            <GalleryStyled onClick={this.galleryItemClick}>
-            {this.state.data !== null &&  <ImageGalleryItem items ={this.state.data} />}
-            </GalleryStyled>
-            {this.state.loading && <Loader /> }
-            </div>
+            {this.state.data !== null && <GalleryStyled onClick={this.galleryItemClick}>
+              <ImageGalleryItem items={this.props.data} />
+            </GalleryStyled>}
+                    </div>
     )
     }
 }
 
-export default ImageGallary;
